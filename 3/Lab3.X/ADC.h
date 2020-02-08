cc6aeda438b33766e4118372526662b2a6489320
JIRS0129 <jirs0129@gmail.com>
2020-02-08 09:43:15 -0600

    ADC
    Multiplexado de ADC con boton para desplegar en PORTD verificar que este funciona.

---
+++ 3/Lab3.X/ADC.h
@@ -0,0 +1,19 @@
+/*
+ * File:   ADC.h
+ * Author: JIRS0129
+ *
+ * Created on February 4, 2020, 8:05 AM
+ */
+
+#ifndef ADC_H
+#define	ADC_H
+#include <stdint.h>
+
+unsigned char adc, adcValue = 0;
+
+void configADC(uint8_t FOSC);
+uint8_t readADC(void);
+void selCanal(uint8_t channel);
+void configCanal(uint8_t channel);
+
+#endif	/* ADC_H */
---
+++ 3/Lab3.X/newmain.c
@@ -0,0 +1,96 @@
+/*
+ * File:   newmain.c
+ * Author: JIRS0129
+ *
+ * Created on February 7, 2020, 10:50 AM
+ */
+
+#pragma config FOSC = INTRC_NOCLKOUT// Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+
+#include <xc.h>
+#include <stdint.h>
+#include "ADC.h"
+
+#define _XTAL_FREQ 4000000
+
+
+uint8_t contADC, sensor1, sensor2 = 0;
+
+void setup(void);
+
+
+
+void __interrupt() ISR(void){
+    if(ADCON0bits.GO_DONE == 0){   
+        adc = 1;
+        PIR1bits.ADIF = 0;
+    }
+}
+
+void main(void) {
+    
+    setup();
+    configADC(1);
+    configCanal(10);
+    configCanal(12);
+    
+    
+    
+    while(1){
+        
+        if(adc){
+            contADC++;
+            if(contADC%2){
+                PORTCbits.RC1 = 1;
+                selCanal(10);
+                sensor1 = readADC();
+            }else{
+                PORTCbits.RC0 = 1;
+                selCanal(12);
+                
+                sensor2 = readADC();
+            }
+            adc = 0;
+        }
+        if(PORTBbits.RB7){
+            PORTCbits.RC2 = 1;
+            PORTD = sensor1;
+        }else{
+            PORTCbits.RC3 = 1;
+            PORTD = sensor2;
+        }
+        if(ADCON0bits.GO_DONE == 0){
+            ADCON0bits.GO_DONE = 1;
+        }
+        
+    }
+    //return;
+}
+
+
+void setup(void){
+    
+    PORTA = 0;
+    PORTB = 0;
+    PORTC = 0;
+    PORTD = 0;
+    PORTE = 0;
+    TRISA = 0;
+    TRISB = 0b10000000;
+    TRISC = 0;
+    TRISD = 0;
+    TRISE = 0;
+}
